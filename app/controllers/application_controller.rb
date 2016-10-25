427ce5dc00c739828fa36cf8cf88f23aa7f6a354
Tommy <twcarrington@gmail.com>
2016-10-25 13:21:01 -0400

    able to see seeded data rendered as json after refactorin controllers with syntax from the responders gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+end
+
+# class ApplicationController < ActionController::Base
+#   protect_from_forgery with: :null_session
+# end
+
+class Api::MyController < ApplicationController
+   def method1
+    # ...
+    respond_to do |format|
+      format.xml { render(xml: "fdsfds") }
+      format.json { render(json: "fdsfdsfd" ) }
+    end
+  end
 end
