6cdb0f1bcb149bbe34d996350920e7959c2442a9
Tommy <twcarrington@gmail.com>
2016-10-25 13:41:16 -0400

    controllers may need some attention regarding class inheritance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+    protect_from_forgery with: :null_session
 end
 
 # class ApplicationController < ActionController::Base
-#   protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
 # end
 
 class Api::MyController < ApplicationController
    def method1
-    # ...
+
     respond_to do |format|
       format.xml { render(xml: "fdsfds") }
       format.json { render(json: "fdsfdsfd" ) }
