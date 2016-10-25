62f6f23f75bd1922821685ea113a2cfd476cb61d
Tommy <twcarrington@gmail.com>
2016-10-25 18:28:33 -0400

    starting to rework app to be purely rails

--- app/controllers/api/v1/proteins_controller.rb
+++ app/controllers/api/v1/proteins_controller.rb
@@ -1,39 +1,12 @@
-# class ProteinsController < ApplicationController
-#   def index
-#     @proteins = Protein.all
-#   end
-#
-#   def new
-#   end
-#
-#   def show
-#     @protein =Protein.find(params[:id])
-#   end
-#
-# end
-
-class Api::V1::ProteinsController < Api::V1::BaseController
+class ProteinsController < ApplicationController
   def index
-    respond_with Protein.all
+    @proteins = Protein.all
   end
 
-  # def create
-  #   respond_with :api, :v1, Protein.create(protein_params)
-  # end
-  #
-  # def destroy
-  #   respond_with Protein.destroy(params[:id])
-  # end
+  def new
+  end
 
-  # def update
-  #   protein = Protein.find(params["id"])
-  #   protein.update_attributes(protein_params)
-  #   respond_with protein, json: protein
-  # end
-
-  # private
-  #
-  # def protein_params
-  #   params.require(:protein).permit(:id, :name, :description)
-  # end
+  def show
+    @protein =Protein.find(params[:id])
+  end
 end
