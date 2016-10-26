9ac9d3ea5c4313dfb638edeb88451fd29bc5cf23
Tommy <twcarrington@gmail.com>
2016-10-25 23:53:59 -0400

    adds new sauce view page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'proteins#index'
     resources :proteins, only: [:index, :show, :new]
-    resources :sauces, only: [:index, :show]
+    resources :sauces, only: [:index, :show, :new]
 end
