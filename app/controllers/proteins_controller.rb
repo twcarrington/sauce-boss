37245605175ad43c36f349beb92e27662296ef1e
Tommy <twcarrington@gmail.com>
2016-10-27 14:08:36 -0400

    removes accidental copypasta of 'article' in controllers, replacing them with 'protein' or 'sauce' as appropriate

--- app/controllers/proteins_controller.rb
+++ app/controllers/proteins_controller.rb
@@ -7,9 +7,18 @@ class ProteinsController < ApplicationController
   end
 
   def create
+    @protein = Protein.new(params[:protein])
+
+    @protein.save
+    redirect_to @proteins
   end
 
   def show
     @protein =Protein.find(params[:id])
+  end
+
+private
+  def protein_params
+    params.require(:protein).permit(:name)
   end
 end
--- app/controllers/sauces_controller.rb
+++ app/controllers/sauces_controller.rb
@@ -7,9 +7,20 @@ class SaucesController < ApplicationController
   end
 
   def create
-  end 
+    # render plain: params[:sauce].inspect
+    @sauce = Sauce.new(params[:sauce])
+
+    @sauce.save
+    # redirect_to @sauce
+    redirect_to @sauces
+  end
 
   def show
     @sauce =Sauce.find(params[:id])
+  end
+
+private
+  def protein_params
+    params.require(:protein).permit(:name, :description)
   end
 end
